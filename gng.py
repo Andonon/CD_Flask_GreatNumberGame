d237dcf2f918299a9245edcdfd063ab3c8e67687
Troy Center <troycenter1@gmail.com>
2017-06-10 17:54:51 -0700

    got the assignment working: bug is where the random does not reset when winning. tomorrow

--- gng.py
+++ gng.py
@@ -2,7 +2,7 @@
 Coding Dojo, Python and Flask fundamentals. Studying session data.
 Troy Center troycenter1@gmail.com June 2017'''
 #pylint: disable=c0111,c0103
-from flask import Flask, render_template, redirect, session, request
+from flask import Flask, render_template, session, request
 
 app = Flask(__name__)
 
@@ -14,13 +14,17 @@ def getrandomnum():
 
 @app.route('/')
 def greatnumbergame():
-    return render_template('gng.html')
+    windisplay = "none"
+    lowdisplay = "none"
+    highdisplay = "none"
+    result = "none"
+    return render_template('gng.html', result=result, windisplay=windisplay, 
+                           lowdisplay=lowdisplay, highdisplay=highdisplay)
 
 @app.route('/checknumbers', methods=['post'])
 def checknumbers():
     print "===line 21 got post info==="
-    print "=== line 22 ===",request.form
-    guess = 0
+    print "=== line 22 ===", request.form
     random = 0
     guess = int(request.form['guess'])
     print "==line 26 Guess is==", guess
@@ -42,16 +46,34 @@ def checknumbers():
         print "===line 39 random is===: ", random
         print "===line 40 guess is ===: ", guess
         print "===line 41 I see a win==="
-        return redirect('/')
+        response = "win"
+        windisplay = "block"
+        lowdisplay = "none"
+        highdisplay = "none"
+        return render_template('gng.html', response=response,
+                               windisplay=windisplay, lowdisplay=lowdisplay,
+                               highdisplay=highdisplay)
     elif random > guess:
         print "===line 43 random is===: ", random
         print "===line 44 guess is ===: ", guess
         print "===line 45 I see you're too low==="
-        return redirect('/')
+        response = "low"
+        windisplay = "none"
+        lowdisplay = "block"
+        highdisplay = "none"
+        return render_template('gng.html', response=response,
+                               windisplay=windisplay, lowdisplay=lowdisplay,
+                               highdisplay=highdisplay)
     elif random < guess:
         print "===line 47 random is===: ", random
         print "===line 48 guess is ===: ", guess
         print "===line 49 I see you're too high.==="
-        return redirect('/')
+        response = "high"
+        windisplay = "none"
+        lowdisplay = "none"
+        highdisplay = "block"
+        return render_template('gng.html', response=response,
+                               windisplay=windisplay, lowdisplay=lowdisplay,
+                               highdisplay=highdisplay)
 
 app.run(debug=True)
